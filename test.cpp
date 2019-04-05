172737709a83f67a2ec43fc231d1a1d03ee51461
Felix Ducret <felix.ducret@ganil.fr>
2019-04-05 10:21:00 +0200

    Test file for vectors

--- test.cpp
+++ test.cpp
@@ -1,30 +1,24 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <cmath>
 
 
-/*
 int main(void)
 {
-	std::string mot1("abc");
-	std::string mot2("abc");
+	std::vector<float> vector1(3,M_PI);
+	std::vector<float> vector2(3,M_E);
 	bool test_unit = true;
 	
-	for(int i=0;i<mot1.size();++i)
+	for(int i=0;i<vector1.size();++i)
 	{
-		if(mot1[i]!=mot2[i])
+		if(vector1[i]!=vector2[i])
 		{
 			test_unit=false;
 			break;
 		}
 	}
 	
-	std::cout << "Les deux string sont-ils les mêmes?" << std::endl << test_unit << std::endl;
+	std::cout << test_unit << std::endl;
 
 }//main
-*/
 
-int main()
-{
-	std::cout << "Hello !" << std::endl;
-	return 0;
-}
