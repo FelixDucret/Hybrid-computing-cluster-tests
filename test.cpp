a9812c4029b1cea050e2d52d75005e9d8ef3bfb5
Felix Ducret <felix.ducret@ganil.fr>
2019-04-05 10:04:09 +0200

    Premier programme de test de strings

---
+++ test.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+
+/*
+int main(void)
+{
+	std::string mot1("abc");
+	std::string mot2("abc");
+	bool test_unit = true;
+	
+	for(int i=0;i<mot1.size();++i)
+	{
+		if(mot1[i]!=mot2[i])
+		{
+			test_unit=false;
+			break;
+		}
+	}
+	
+	std::cout << "Les deux string sont-ils les mêmes?" << std::endl << test_unit << std::endl;
+
+}//main
+*/
+
+int main()
+{
+	std::cout << "Hello !" << std::endl;
+	return 0;
+}
