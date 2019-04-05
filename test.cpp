b4bc2a8d84843d28640112020ab4a945f548c3c1
Felix Ducret <felix.ducret@ganil.fr>
2019-04-05 16:27:23 +0200

    Translation of words from french to english

--- test.cpp
+++ test.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main(void)
 {
-	string mot1("abc");
-	string mot2("bcd");
+	string word1("abc");
+	string word2("bcd");
 	bool test_unit = true;
 	
-	for(int i=0;i<mot1.size();++i)
+	for(int i=0;i<word1.size();++i)
 	{
-		if(mot1[i]!=mot2[i])
+		if(word1[i]!=word2[i])
 		{
 			test_unit=false;
 			break;
