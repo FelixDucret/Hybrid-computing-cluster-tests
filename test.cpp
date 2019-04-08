bdbc7dbcb26b760195e9b06be2b10070af5dc2f8
Felix Ducret <felix.ducret@ganil.fr>
2019-04-08 10:31:26 +0200

    Add: test version for vector summation

--- test.cpp
+++ test.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
-#include <string>
+#include <vector>
+
 using namespace std;
+ 
+
+vector<int> sum_vectors(vector<int> vec1, vector<int> vec2)
+ {
+	vector<int> sum(vec1.size());
+	if(vec1.size()!=vec2.size())
+	{
+		printf("Vectors can't be sumed : different lengths\n);
+		return  sum;
+	}	
+	for(int i=0; i<vec1.size(); i++)
+ 	{
+		sum.push_back(vec1[i]+vec2[i]);
+ 	}
+	
+
+	return sum;
+}
+ 
 
 int main(void)
 {
-	string mot1("abc");
-	string mot2("bcd");
-	bool test_unit = true;
+	vector<int> vec1(5,2);
+	vector<int> vec2(5,3);
 	
-	for(int i=0;i<mot1.size();++i)
-	{
-		if(mot1[i]!=mot2[i])
-		{
-			test_unit=false;
-			break;
-		}
-	}
-	
-	std::cout << test_unit << std::endl;
+	cout << sum_vectors(vec1,vec2) << endl;
 
 }//main
 
