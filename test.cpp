3da1af13f6e11be127b6192ff705a9f6b5ba18b3
Felix Ducret <felix.ducret@ganil.fr>
2019-04-08 10:50:50 +0200

    Correction of errors

--- test.cpp
+++ test.cpp
@@ -6,28 +6,31 @@ using namespace std;
 
 vector<int> sum_vectors(vector<int> vec1, vector<int> vec2)
  {
-	vector<int> sum(vec1.size());
+	vector<int> sum;
 	if(vec1.size()!=vec2.size())
 	{
-		printf("Vectors can't be sumed : different lengths\n);
+		cout << "Vectors can't be sumed : different lengths" << endl;
 		return  sum;
 	}	
-	for(int i=0; i<vec1.size(); i++)
+	for(int i=0; i<vec1.size(); ++i)
  	{
 		sum.push_back(vec1[i]+vec2[i]);
  	}
-	
-
 	return sum;
 }
  
 
 int main(void)
 {
-	vector<int> vec1(5,2);
+	vector<int> vec1(4,2);
 	vector<int> vec2(5,3);
+
+	vector<int> S=sum_vectors(vec1,vec2);
 	
-	cout << sum_vectors(vec1,vec2) << endl;
+	for(int i=0; i<S.size(); i++)
+	{	
+		cout << S[i] << endl;
+	}
 
 }//main
 
