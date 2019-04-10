e2d9364d30b8bb03f90fc280a9ef308aa4672f5d
Felix Ducret <felix.ducret@ganil.fr>
2019-04-10 13:38:23 +0200

    Working vectors sumation program using Boost.Program_options library

--- test.cpp
+++ test.cpp
@@ -1,6 +1,9 @@
 #include <boost/program_options.hpp>
 #include <iostream>
 #include <iterator>
+#include <vector>
+
+using namespace std;
 
 struct ret{
 	bool e;	//Error bool
@@ -35,29 +38,71 @@ ret prog_options(int ac, char* av[])
 	po::notify(vm);    
 
 	if (vm.count("help")) {
-    		std::cout << desc << "\n";
+    		cout << desc << "\n";
     		result.e = 1;
-	}
+	}//if
 
-	else if (vm.count("length")) {
-    		std::cout << "You chose to sum the " 
- 		<< vm["length"].as<int>() << " first terms of the vectors.\n";
-		result.l=vm["length"].as<int>();
-
-	} else {
-    		std::cout << "You chose to sum all the terms of the vectors.\n";
-	}
-	
+	if (vm.count("length")) {
+    		result.l=vm["length"].as<int>();
+	}//if
+    		
 	
 	return result;
+}//prog_options
+
+//Template for vectors sumation
+template <typename T>
+vector<T> add(const vector<T> &a, const vector<T> &b, vector<T> &c)
+{
+	vector<T> sum;
+        if(a.size()!=b.size()||b.size()!=c.size())
+        {
+              cout << "Vectors can't be sumed : different lengths" << endl;
+              return sum;
+        }
+	else
+ 	{
+		for(int i=0; i<a.size(); ++i)
+        	{
+                	sum.push_back(a[i]+b[i]+c[i]);
+        	}
+	}
+ 	return sum;
 }
 
 int main(int ac, char* av[])
 {
 	ret s=prog_options(ac, av);
-	
-	
 
+	vector<int>vec1 (5,2);
+	vector<int>vec2 (5,3);
+	vector<int>vec3 (5,4);
+	
+	vector<int> S = add(vec1,vec2,vec3);
+	if(s.e==0)
+	{
+		if(s.l>vec1.size())
+        	{
+                	cout << "Vectors too short, last elements are not defined" << endl;
+        	}//if
+
+		else
+		{
+			if(s.l!=0)
+        		{
+        			for(int i=0; i<s.l; i++)
+        	        		{	
+        	        			cout << S[i] << endl;
+        	        		}//for
+        		}//if
+        		else{
+        			for(int i=0; i<S.size(); i++)
+        			{	
+        	    			cout << S[i] << endl;
+        			}//for
+        		}//else2
+		}//else1
+	}//if
 	return s.e;
 
 }//main
