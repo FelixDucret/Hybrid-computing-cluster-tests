fe6cd0608f8f4923b4a3fc22489e57b0b75c2869
Felix Ducret <ducretf@ganil.fr>
2019-04-30 10:45:58 +0000

    Add: Program options with Boost.Program_options library

--- Makefile
+++ Makefile
@@ -8,8 +8,8 @@ else
 endif
 
 all: testBoostCompute.cpp
-	g++ -Wall -g testBoostCompute.cpp -o test $(OPTIONS) -DBOOST_COMPUTE_MAX_CL_VERSION=102
-	./test
+	g++ -Wall -g testBoostCompute.cpp -o test $(OPTIONS) -DBOOST_COMPUTE_MAX_CL_VERSION=102 -lboost_program_options
+	./test -l 10
 
 clean:
 	rm -rf test
--- testBoostCompute.cpp
+++ testBoostCompute.cpp
@@ -1,12 +1,53 @@
 #include <vector>
 #include <algorithm>
+#include <iostream>
 #include <boost/compute.hpp>
+#include <boost/program_options.hpp>
 
 namespace compute = boost::compute;
+namespace po = boost::program_options;
 
-int main()
+int prog_options(int ac, char* av[])
 {
-    int VECTOR_SIZE = 16384;			//Maximum size (for now) : 16384
+	int ret;
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help,h", "produce help message")
+		("length,l", po::value<int>(), "choose the length of the vector that will be sorted")
+	;
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(ac, av, desc), vm);
+
+	po::notify(vm);
+
+
+	if (vm.count("help")) {
+		std::cout << desc << "\n";
+	}
+
+	if (vm.count("length")) {
+		ret=vm["length"].as<int>();
+	}
+
+	return ret;
+}
+
+
+int main(int ac, char* av[])
+{
+    int VECTOR_SIZE = prog_options(ac, av);	//Maximum size (for now) : 16384
+
+    //tests on VECTOR_SIZE to see it has a right value
+    while (VECTOR_SIZE<0 || VECTOR_SIZE>16384)
+    {
+	std::cout << "Please enter a valid value (cannot be superior to 16384) : " << std::endl;
+	std::cin >> VECTOR_SIZE;
+    }
+
+    if(VECTOR_SIZE==0)
+	return 0;
+
     // get the default compute device
     compute::device gpu = compute::system::default_device();
 
@@ -35,6 +76,13 @@ int main()
     compute::copy(
         device_vector.begin(), device_vector.end(), host_vector.begin(), queue
     );
+    int err = 0;
+    for(int i=0; i<host_vector.size()-1; ++i)
+    {
+	if(host_vector[i]>host_vector[i+1])
+		++err;
+    }
+    std::cout << "Errors : " << err << std::endl;
 
     return 0;
 }
