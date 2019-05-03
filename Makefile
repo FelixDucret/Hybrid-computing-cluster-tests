da20ecf3eb7482ed6813055211520e0ae3db5315
Felix Ducret <ducretf@ganil.fr>
2019-05-03 13:39:46 +0000

    Can now change the values of pixels in the image

--- Makefile
+++ Makefile
@@ -1,4 +1,9 @@
-all:
-	g++ -o test test.cpp -Wall -W -lm -ansi -pedantic -O1 -fno-tree-pre -Dcimg_use_vt100 -Dcimg_use_xshm -Dcimg_use_xrandr -lpthread -lX11 -lXext -lXrandr
+all: display nodisplay
+
+display:
+	g++ -o test.X test.cpp -Wall -W -lm -ansi -pedantic -O3 -fno-tree-pre -Dcimg_use_vt100 -Dcimg_use_xshm -Dcimg_use_xrandr -lpthread -lX11 -lXext -lXrandr -Dwindows -I/home/rock64/testGlobal/CImg
+	./test.X
+
 nodisplay:
-	g++ test.cpp -o test -Wall -W -lm -ansi -pedantic -O0 -fno-tree-pre -Dcimg_use_vt100 -lpthread -Dcimg_display=0
+	g++ test.cpp -o test -Wall -W -lm -ansi -pedantic -O0 -fno-tree-pre -Dcimg_use_vt100 -lpthread -Dcimg_display=0 -I/home/rock64/testGlobal/CImg
+	./test
--- test.cpp
+++ test.cpp
@@ -1,18 +1,30 @@
 #include <CImg.h>
+#include <iostream>
 using namespace cimg_library;
 
+
 int main() {
-  CImg<unsigned char> image("Image_test.png"), visu(500,400,1,1,0),image2("Image_test-2.png");
+  CImg<unsigned char> image("Image_test.png"), visu(500,400,1,1,0);//,image2("Image_test-2.png");
+  //CImg<unsigned char> image3 = image+image2;
+
+  unsigned char* pix=image.data();
+  for(int i=0; i<1000; ++i)
+	*(pix+i+15000)=255;
+  //std::cout << "Image size : " << image.size() << std::endl;
+  //std::cout << "Image width : " << image.width() << std::endl;
+  //std::cout << "Image height : " << image.height() << std::endl;
+
+
+#ifdef windows
   const unsigned char white[] = { 255,255,255 };
-  CImg<unsigned char> image3(640,400,1,1,0);
-  image3 = image+image2;
-  CImgDisplay main_disp(image3,"Click a point"), draw_disp(visu,"Grayscale level");
+  CImgDisplay main_disp(image,"Click a point"), draw_disp(visu,"Grayscale level");
   while (!main_disp.is_closed() && !draw_disp.is_closed()) {
     main_disp.wait();
     if (main_disp.button() && main_disp.mouse_y()>=0) {
       const int y = main_disp.mouse_y();
-      visu.fill(0).draw_graph(image3.get_crop(0,y,0,0,image3.width()-1,y,0,0),white,1,1,0,255,0).display(draw_disp);
+      visu.fill(0).draw_graph(image.get_crop(0,y,0,0,image.width()-1,y,0,0),white,1,1,0,255,0).display(draw_disp);
       }
     }
+#endif
   return 0;
 }
