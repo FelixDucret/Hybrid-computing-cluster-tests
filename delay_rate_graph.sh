ddf899087193276d476e867ca99730420be63155
Felix Ducret <ducretf@ganil.fr>
2019-06-24 12:55:39 +0000

    Now displays the number of the last packet received

--- delay_rate_graph.sh
+++ delay_rate_graph.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-echo $1 $2 $3 $4
+#echo $1 $2 $3 $4
 /home/rock64/UDP_multiport/udp_server $1 $2 $3 $4 > temp &
 ssh ducretf@193.48.111.54 'echo Hi && ~/UDP_multiport/udp_client 10.10.15.1  1234567 1 4096 block 100000  50000 3210 & && exit '
-cat temp && echo
+sleep 1
+cat temp | rev | cut -d'#' -f 1 | rev > temp2
 killall udp_server
+cat temp2
 
